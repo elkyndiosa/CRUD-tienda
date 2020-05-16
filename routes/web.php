bb2ed16d95fb70cbe6d4a68eaf4dd8bdf10be20a
elkyndiosa <elkyndiosa23@gmail.com>
2020-05-16 10:08:07 -0500

    prueba

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Auth::routes();
-Route::get('/{option?}/{opt?}', function () {
+Route::get('/{option?}/{opt2?}/{opt3?}', function () {
     return view('home');
 })->middleware('auth');
-
-
